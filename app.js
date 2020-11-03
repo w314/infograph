a1ea03763e3d6ca2b12b3dc7728b6251c862e38b
Piroska Werner <piroska.werner@gmail.com>
2020-11-02 19:26:48 -0600

    feat: Add dino and human objects

--- app.js
+++ app.js
@@ -1,11 +1,110 @@
-
+function gameOn() {
     // Create Dino Constructor
+
+    function Dino(species, weight, height, diet, where, when, fact) {
+        this.name = species;
+        this.weight = weight;
+        this.height = height;
+        this.diet = diet;
+        this.location = when;
+        this.time = where;
+        this.fact = fact;
+    }
 
 
     // Create Dino Objects
 
+    const dinoFacts = [
+        {
+            "species": "Triceratops",
+            "weight": 13000,
+            "height": 114,
+            "diet": "herbavor",
+            "where": "North America",
+            "when": "Late Cretaceous",
+            "fact": "First discovered in 1889 by Othniel Charles Marsh"
+        },
+        {
+            "species": "Tyrannosaurus Rex",
+            "weight": 11905,
+            "height": 144,
+            "diet": "carnivor",
+            "where": "North America",
+            "when": "Late Cretaceous",
+            "fact": "The largest known skull measures in at 5 feet long."
+        },
+        {
+            "species": "Anklyosaurus",
+            "weight": 10500,
+            "height": 55,
+            "diet": "herbavor",
+            "where": "North America",
+            "when": "Late Cretaceous",
+            "fact": "Anklyosaurus survived for approximately 135 million years."
+        },
+        {
+            "species": "Brachiosaurus",
+            "weight": 70000,
+            "height": "372",
+            "diet": "herbavor",
+            "where": "North America",
+            "when": "Late Jurasic",
+            "fact": "An asteroid was named 9954 Brachiosaurus in 1991."
+        },
+        {
+            "species": "Stegosaurus",
+            "weight": 11600,
+            "height": 79,
+            "diet": "herbavor",
+            "where": "North America, Europe, Asia",
+            "when": "Late Jurasic to Early Cretaceous",
+            "fact": "The Stegosaurus had between 17 and 22 seperate places and flat spines."
+        },
+        {
+            "species": "Elasmosaurus",
+            "weight": 16000,
+            "height": 59,
+            "diet": "carnivor",
+            "where": "North America",
+            "when": "Late Cretaceous",
+            "fact": "Elasmosaurus was a marine reptile first discovered in Kansas."
+        },
+        {
+            "species": "Pteranodon",
+            "weight": 44,
+            "height": 20,
+            "diet": "carnivor",
+            "where": "North America",
+            "when": "Late Cretaceous",
+            "fact": "Actually a flying reptile, the Pteranodon is not a dinosaur."
+        },
+        {
+            "species": "Pigeon",
+            "weight": 0.5,
+            "height": 9,
+            "diet": "herbavor",
+            "where": "World Wide",
+            "when": "Holocene",
+            "fact": "All birds are living dinosaurs."
+        }
+    ]
 
-    // Create Human Object
+    // create array of Dino objects based on dinoFacts
+    const dinos = dinoFacts.map(function (dino) {
+        return (
+            new Dino(dino.species, dino.weight, dino.height, dino.diet, dino.where, dino.when, dino.fact)
+        )
+    })
+
+
+    // Create Human Object based on From Input
+    const human = {
+        name: document.getElementById('name').value,
+        weight: document.getElementById('weight').value,
+        // store height in inches
+        height: document.getElementById('feet').value * 12 + document.getElementById('inches').value * 1,
+        diet: document.getElementById('diet').value
+    };
 
     // Use IIFE to get human data from form
 
@@ -28,5 +127,6 @@
 
     // Remove form from screen
 
+}
 
 // On button click, prepare and display infographic
